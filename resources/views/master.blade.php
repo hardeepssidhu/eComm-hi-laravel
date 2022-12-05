3ed97497b99aff7edf2b163f1b98e5b30d3ef962
hardeepssidhu <hardeepsidhu00011@gmail.com>
2022-12-05 16:36:10 +0530

    login page done

---
+++ resources/views/master.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>E-comm Project</title>
+
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</head>
+<body>
+
+    {{View::make('header')}}
+        @yield('content')
+    {{View::make('footer')}}
+
+</body>
+
+<!-- <script>
+    $(document).ready(function()
+    {
+        $("button").click(function(){
+            alert("all set")
+        })
+
+    })
+</script> -->
+    <style>
+        .custom-login {
+            height: 500px;
+            padding-top: 100px;
+        }
+
+
+    </style>
+
+</html>
